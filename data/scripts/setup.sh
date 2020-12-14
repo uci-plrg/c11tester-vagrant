a59f6154cbf0e6299b81143cdf2171ce12c8fff0
weiyu <weiyuluo1232@gmail.com>
2020-12-13 18:43:44 -0800

    Update readme

--- data/scripts/setup.sh
+++ data/scripts/setup.sh
@@ -5,7 +5,7 @@ cd ~
 # 1. Getting and compiling llvm
 sh /vagrant/data/scripts/get-llvm.sh
 
-# 1. Getting and compiling c11tester
+# 2. Getting and compiling c11tester
 git clone git://plrg.eecs.uci.edu/c11tester.git
 cd ~/c11tester
 git checkout vagrant
@@ -13,15 +13,17 @@ make clean
 make
 cd ..
 
-# 2. Benchmarks
+# 3. Benchmarks
 git clone git://plrg.eecs.uci.edu/c11concurrency-benchmarks.git c11tester-benchmarks
 cd c11tester-benchmarks
 git checkout vagrant
 cp /vagrant/data/scripts/build.sh .
+cp /vagrant/data/scripts/test_all.sh .
+cp /vagrant/data/scripts/run.sh .
 sh build.sh
 cd ..
 
-## Firefox
+## Firefox Javascript shell
 cp /vagrant/data/scripts/build_firefox_jsshell.sh .
 sh build_firefox_jsshell.sh
 echo >&2 "Setup is now complete. To run the benchmarks, please look at our READE.md"
